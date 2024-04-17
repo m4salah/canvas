593957e585b1d888ff6052b35548ce0aa9fd2069
Mohamed A. Salah <mohamed.a.salah11@gmail.com>
2024-04-17 04:26:27 +0200

    Initial commit

---
+++ cmd/server/main.go
@@ -0,0 +1,11 @@
+// Package main is the entry point to the server. It reads configuration, sets up logging and error handling,
+// handles signals from the OS, and starts and stops the server.
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("🤓")
+}
