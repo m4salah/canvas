aff16df377a4ccb105753cb308e2a7b443ca8234
Mohamed A. Salah <mohamed.a.salah11@gmail.com>
2024-04-19 18:38:51 +0200

    add news letter handler and tests

--- server/routes.go
+++ server/routes.go
@@ -2,10 +2,22 @@ package server
 
 import (
 	"canvas/handlers"
+	"canvas/model"
+	"context"
 )
+
+type signupperMock struct{}
+
+func (s signupperMock) SignupForNewsletter(ctx context.Context, email model.Email) (string, error) {
+	return "", nil
+}
 
 func (s *Server) setupRoutes() {
 	handlers.Static(s.mux)
 	handlers.Health(s.mux)
 	handlers.Home(s.mux)
+
+	// newsletter routes
+	handlers.NewsletterSignup(s.mux, signupperMock{})
+	handlers.NewsletterThanks(s.mux)
 }
